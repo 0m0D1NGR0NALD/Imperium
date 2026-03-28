70ac7b2793810b298350e96aef3777d2fbfd96ed
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2026-03-28 23:38:31 +0100

    uploading transaction routes

---
+++ packages/backend/src/routes/transactionRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const auth = require('../middleware/auth');
+const { getTransactions, createTransaction, updateTransaction, deleteTransaction } = require('../controllers/transactionController');
+const router = express.Router();
+
+router.use(auth);
+router.get('/', getTransactions);
+router.post('/', createTransaction);
+router.put('/:id', updateTransaction);
+router.delete('/:id', deleteTransaction);
+
+module.exports = router;
