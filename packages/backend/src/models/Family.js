39a6fa1986691e8f276c174b1d16558b32046d20
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2026-04-05 18:29:12 +0200

    Add inviteCode to Family model

--- packages/backend/src/models/Family.js
+++ packages/backend/src/models/Family.js
@@ -3,7 +3,8 @@ const sequelize = require('../config/database');
 
 const Family = sequelize.define('Family', {
   id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-  name: { type: DataTypes.STRING, allowNull: false }
+  name: { type: DataTypes.STRING, allowNull: false },
+  inviteCode: { type: DataTypes.STRING, unique: true }
 });
 
 module.exports = Family;
